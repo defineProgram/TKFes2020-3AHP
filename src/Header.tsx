c31b345f11a4b963be771616ecad75bb35be29a2
define <defineprogram@gmail.com>
2020-10-16 07:32:56 +0900

    improve menu

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,43 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import './Header.css'
 
-interface Props{
-	name?:string;
+interface Props {
+	name?: string;
 }
-function Header(props:Props) {
+function Header(props: Props) {
 	const [isOpen, setisOpen] = useState(false);
-	const [isHidden,setisHidden]=useState(false);
-	const [pos1,setpos1]=useState(window.pageYOffset);
-	const [pos2,setpos2]=useState(window.pageYOffset);
-	const onScroll=(()=>{
-		if(isOpen)return;
-		if(!isHidden&&window.pageYOffset-pos1>=100&&window.pageYOffset>=400){
+	const [isHidden, setisHidden] = useState(false);
+	const [pos1, setpos1] = useState(window.pageYOffset);
+	const [pos2, setpos2] = useState(window.pageYOffset);
+	const onScroll = (() => {
+		if (isOpen) return;
+		if (!isHidden && window.pageYOffset - pos1 >= 100 && window.pageYOffset >= 400) {
 			setisHidden(true);
 			setpos1(window.pageYOffset);
 			setpos2(window.pageYOffset);
-		}else if(!isHidden){
-			setpos1(Math.min(pos1,window.pageYOffset));
+		} else if (!isHidden) {
+			setpos1(Math.min(pos1, window.pageYOffset));
 		}
-		if(isHidden&&pos2-window.pageYOffset>=100){
+		if (isHidden && pos2 - window.pageYOffset >= 100) {
 			setisHidden(false);
 			setpos1(window.pageYOffset);
 			setpos2(window.pageYOffset);
-		}else if(isHidden){
-			setpos2(Math.max(pos2,window.pageYOffset));
+		} else if (isHidden) {
+			setpos2(Math.max(pos2, window.pageYOffset));
 		}
 	})
-	const onKeyDown=(()=>{if(isOpen)setisOpen(false)});
-	useEffect(()=>{
+	const onKeyDown = (() => { if (isOpen) setisOpen(false) });
+	useEffect(() => {
 		document.addEventListener("scroll", onScroll);
-		document.addEventListener("click",onKeyDown);
-    	return (): void => {
+		document.addEventListener("click", onKeyDown);
+		return (): void => {
 			document.removeEventListener("scroll", onScroll);
-			document.removeEventListener("click",onKeyDown);
+			document.removeEventListener("click", onKeyDown);
 		}
 	});
 	return (
-		<header className={(isHidden?"hidden":"tapu")}>
-			<div className={"hideLayer "+(isOpen?"active":"inactive")} />
+		<header className={(isHidden ? "hidden" : "tapu")}>
+			<div className={"hideLayer " + (isOpen ? "active" : "inactive")} />
 			<div className={"humburger " + (isOpen ? "active" : "inactive")}
 				onClick={() => setisOpen(!isOpen)}>
 				<span></span>
@@ -45,6 +45,8 @@ function Header(props:Props) {
 				<span></span>
 			</div>
 			<nav className={"globalMenuSp" + " " + (isOpen ? "active" : "inactive")}>
+				<div className="decotitle">筑駒文化祭2020 3AHR</div>
+				<div className="decosubtitle">ー今から皆さんは観客ですー</div>
 				<ul>
 					<li><a href="/">Home</a></li>
 					<li><a href="/About">About</a></li>
